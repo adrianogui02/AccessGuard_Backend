361269e60e3af53a8fb397b5545f68281c1eb389
Adriano Nobre <adrianonobrevg02@gmail.com>
2024-05-16 20:35:12 -0400

    Update residentsRoutes.js

--- routes/residentsRoutes.js
+++ routes/residentsRoutes.js
@@ -6,5 +6,6 @@ const residentsController = require("../controllers/residentsController");
 router.post("/create", residentsController.addResident);
 router.get("/getByUser/:userId", residentsController.getResidentsByUser);
 router.get("/getAllResidents", residentsController.getAllResidents);
+router.delete("/delete/:id", residentsController.deleteResident);
 
 module.exports = router;
